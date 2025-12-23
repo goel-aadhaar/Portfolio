01901307858701537c2b86512a60cd3ee01650f5
goel-aadhaar <goel.aadhaar@outlook.com>
2025-12-23 05:37:52 +0530

    fix prisma error

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,6 @@
-import { PrismaClient , Prisma } from '@/app/generated/prisma/client';
+import { PrismaClient, Prisma } from "../app/generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg'
-import 'dotenv/config';
-import { connect } from 'http2';
+import 'dotenv/config'
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL || '',
