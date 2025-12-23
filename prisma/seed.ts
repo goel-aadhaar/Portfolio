bb0eeb8f59a7c0ce2178ad91794f783a766d56f6
goel-aadhaar <goel.aadhaar@outlook.com>
2025-12-23 06:24:23 +0530

    try to fix error in prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "../app/generated/prisma/client.ts";
+import { PrismaClient, Prisma } from "../app/generated/prisma";
 import { PrismaPg } from '@prisma/adapter-pg'
 import 'dotenv/config'
 
